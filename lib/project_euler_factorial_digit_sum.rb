92ef0cf77df0a6c07937e948ba00595bec499019
mikeappell <mikeappell@gmail.com>
2015-11-06 16:08:02 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,11 @@
-# code your solution here
+def factorial(num)
+  (1..num).inject(:*)
+end
+
+def sum_of_digits(num)
+  num.to_s.chars.inject(0) {|sum, char| sum += char.to_i}
+end
+
+def factorial_digit_sum(num)
+  sum_of_digits(factorial(num))
+end
